69a626da20228fa5402f9b8c6e9d4388d411c2e4
alburokyla <alburo.kn@ciccebu.edu.ph>
2025-05-23 18:59:18 +0800

    Create FinalExam_2300255.c

---
+++ FinalExam_2300255.c
@@ -0,0 +1,22 @@
+// File name: finalexam_2300255.c
+// Name: Kyla Nicole Alburo
+// Student ID: 2300255
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int rows = 5;
+
+
+    for (i = rows; i >= 1; i--) {
+
+    for (j = 1; j <= i; j++) {
+    printf("*");
+    }
+
+    printf("\n");
+    }
+
+    return 0;
+}
